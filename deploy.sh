58dfa9f46780b07534964cac0b8af59020d2d596
Tusheet Goli <tgoli@yugabyte.com>
2021-11-06 12:13:26 -0400

    fix script file

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
-#heroku login;
 docker build -t task-service .;
 docker tag task-service registry.heroku.com/cs4261-task-service/web;
 docker push registry.heroku.com/cs4261-task-service/web;
 heroku container:release web -a cs4261-task-service;
-#heroku logs --tail --app cs4261-task-service;
